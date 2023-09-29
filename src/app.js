d2c748f146862704922d8398beee97072a541a72
Sofia Hojberg <sofiahojberg@gmail.com>
2023-09-28 23:00:25 -0300

    Set sequelize config

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const cookies = require('cookie-parser');
 
 const mainRoutes = require('./routes/mainRoutes');
 const usersRoutes = require('./routes/usersRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
@@ -14,7 +18,18 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
+app.use(session({
+    secret:"It's a secret",
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(cookies());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(methodOverride('_method'));
+
 app.use('/', mainRoutes);
+app.use('/admin', adminRoutes)
 app.use('/user', usersRoutes)
 
 app.listen(PORT, () => console.log("Listening on http://localhost:" + PORT));
